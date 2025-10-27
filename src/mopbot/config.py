bd50abcee01e2652a7e10642503c1e377672a0ad
Morpheus636 <morpheus636@morpheus636.com>
2025-10-27 23:52:10 +0000

    Switch to JSONschema for validation

--- src/mopbot/config.py
+++ src/mopbot/config.py
@@ -1,4 +1,10 @@
+import importlib
+import json
+
 import yaml
+import jsonschema
+
+from . import data
 
 config = []
 
@@ -6,31 +12,7 @@ config = []
 with open("config.yaml") as stream:
     config = yaml.safe_load(stream);
 
-
 def validate(config):
-    # Guild ID is required
-    assert type(config.get("guild_id")) is int
-
-    # ROLES MODULE
-    roles = config.get("roles")
-    if roles is not None:
-        # Top level item under roles should be a list.
-        assert type(roles) is list
-        for i in roles:
-            # Each item under roles should be a dictionary.
-            assert type(i) is dict
-
-            # The dictionary should contain two lists.
-            role_ids = i["role_ids"]
-            perms = i["permissions"]
-            assert type(role_ids) is list
-            assert type(perms) is dict
-
-            # Each item in the role_ids list should be an int.
-            for j in role_ids:
-                assert type(j) is int
-    
-            # Each pair in the perms list should be a pair of str, bool
-            for j in perms:
-                assert type(j) is str
-                assert type(perms[j]) is bool
+    with open(importlib.resources.files(data)/"schema.json") as stream:
+        schema = json.load(stream)
+    jsonschema.validate(config, schema)
