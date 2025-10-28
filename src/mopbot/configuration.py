95cf2fba336c8208a83833b4a33acec3e2f63d5a
Morpheus636 <morpheus636@morpheus636.com>
2025-10-28 15:08:39 +0000

    code formatting

--- src/mopbot/configuration.py
+++ src/mopbot/configuration.py
@@ -9,15 +9,17 @@ from . import data
 
 logger = logging.getLogger(__name__)
 
+
 def load(file) -> dict:
     """Load the specified configuration file from disk."""
     logger.info(f"Loading config file: {file}")
     with open(file) as stream:
         return yaml.safe_load(stream)
 
+
 def validate(config) -> None:
     logger.debug("Loading JSON Schema for config validation.")
-    with open(importlib.resources.files(data)/"schema.json") as stream:
+    with open(importlib.resources.files(data) / "schema.json") as stream:
         schema = json.load(stream)
 
     jsonschema.validate(config, schema)
--- src/mopbot/roles.py
+++ src/mopbot/roles.py
@@ -4,6 +4,7 @@ import discord
 
 logger = logging.getLogger(__name__)
 
+
 async def apply_perms(client, roles, env):
     """Overwrites roles' permissions with the ones configured by the bot."""
     guild = client.get_guild(env["guild_id"])
@@ -13,7 +14,7 @@ async def apply_perms(client, roles, env):
             new_perms = discord.Permissions()
             for perm in configured_perms:
                 setattr(new_perms, perm, configured_perms[perm])
-    
+
             role_id = env["roles"][role]
             logger.info(f"Updating role '{role}' (Role ID: {role_id}): {new_perms}")
             await guild.get_role(role_id).edit(permissions=new_perms)
