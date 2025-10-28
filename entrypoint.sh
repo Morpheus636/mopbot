139474fa87e5d7e04de0e36de47fcd16b3eb4161
Morpheus636 <morpheus636@morpheus636.com>
2025-10-28 18:45:05 +0000

    Added command-line args as inputs to github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
-
+set -e
 cd /github/workspace
-poetry --project=/app run python -m mopbot $1
+
+CONFIG_FILE="$1"
+CHECK="$2"
+DRY_RUN="$3"
+
+ARGS=""
+
+if [ -n "$CONFIG_FILE" ]; then
+  ARGS="$ARGS $CONFIG_FILE"
+fi
+
+if [ "$CHECK" = "true" ]; then
+  ARGS="$ARGS --check"
+fi
+
+if [ "$DRY_RUN" = "true" ]; then
+  ARGS="$ARGS --dry_run"
+fi
+
+poetry --project=/app run python -m mopbot $ARGS
